1deaac83326442f25a414e1104f5a95c8df98f36
justindglee <34882520+nninnnin@users.noreply.github.com>
2021-07-10 17:15:41 +0900

    Time: 128 ms (5.42%), Space: 46.7 MB (5.15%) - LeetHub

---
+++ divisor-game/divisor-game.js
@@ -0,0 +1,37 @@
+var divisorGame = function (N) {
+  const DP = [];
+  DP[1] = false;
+  
+  function checkGameResult (num) {
+    let result = false;
+
+    if (DP[num] !== undefined) {
+      console.log(`DP for ${num} = ${DP[num]} has used!`);
+      return DP[num];
+    }
+
+    const selectedNumbers = selectNumbers(num);
+
+    for (let i = 0; i < selectedNumbers.length; i++) {
+      result = result || !checkGameResult(num - selectedNumbers[i]);
+
+      DP[num] = result;
+    }
+    
+    return result;
+  }
+
+  function selectNumbers (N) {
+    const selected = [];
+
+    for (let num = 1; num < N; num++) {
+      if (N % num === 0) {
+        selected.push(num);
+      }
+    }
+
+    return selected;
+  }
+
+  return checkGameResult(N);
+};
