89367ea2645e452e591c8e1bb5dfa9691cc52ab2
justindglee <34882520+nninnnin@users.noreply.github.com>
2021-07-18 12:24:09 +0900

    Time: 102 ms (16.91%), Space: 38.8 MB (69.06%) - LeetHub

---
+++ kth-missing-positive-number/kth-missing-positive-number.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+  const largestNum = arr[arr.length - 1];
+
+  for (let i = 1; i < largestNum; i++) {
+      if (!arr.includes(i)) k--;
+
+      if (k === 0) return i;
+  }
+
+  return largestNum + k;
+};
