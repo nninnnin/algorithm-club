5d44a200a01917cac861a6c290ef81d8b1a70f03
justindglee <34882520+nninnnin@users.noreply.github.com>
2021-07-25 11:09:09 +0900

    Time: 190 ms (33.54%), Space: 50.1 MB (33.83%) - LeetHub

--- find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
+++ find-all-numbers-disappeared-in-an-array/find-all-numbers-disappeared-in-an-array.js
@@ -6,13 +6,18 @@ var findDisappearedNumbers = function(nums) {
     const cache = {};
     const result = [];
     
+    // O(n)
+    // 먼저 있는 숫자들을 해시테이블에 넣어준다
     nums.forEach(num => {
         cache[num] = true;
     });
     
+    // O(n)
+    // 1부터 순회하며 없는 숫자들을 result에 푸시해준다
     for (let i = 1; i <= nums.length; i++) {
         if (!cache[i]) result.push(i);
     }
     
+    // 결과적으로 O(2n) 의 시간복잡도
     return result;
 };
